8a37119e0ee323eb1fee0460ce247f3a2754e08a
Mila Myers <Amelia.myers@live.com>
2025-02-04 15:50:08 -0700

    Create plotting_exercise.py

---
+++ plotting_exercise.py
@@ -0,0 +1,47 @@
+# !/usr/bin/python3.12.3
+
+#####################################
+#
+# Class 5: Linear and Log + Plotting
+# Author: Amelia Myers
+#
+#####################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import Myers_function_lab as mfl 
+
+#generating data to use for the plots
+x = np.linspace(1, 100, 500)
+y = mfl.y(x)
+
+#transforming x-yspace into logspace
+log_y = np.log10(y)
+
+log_x = np.log10(x)
+
+#creation of 3 plots in 1 window
+fig, ax = plt.subplots(1, 3, figsize=(10,6))
+#linear plot
+ax[0].plot(x, y, label='Linear Plot', color='r', linewidth=5)
+ax[0].set_xlabel('x')
+ax[0].set_ylabel('y')
+ax[0].set_title('Linear Plot')
+ax[0].grid(True, linestyle='--')
+
+#log-log plot
+ax[1].plot(x, log_y, label='Log-Log Plot', color='magenta', linewidth=5)
+ax[1].set_xlabel('x')
+ax[1].set_ylabel('log(y)')
+ax[1].set_title('Log-Log Plot')
+ax[1].grid(True, linestyle='--')
+
+#log(x_ vs log(y)
+ax[2].plot(log_x, log_y, label='Log(x) vs Log(y)', color="purple", linewidth=5)
+ax[2].set_xlabel('log(x)')
+ax[2].set_ylabel('log(y)')
+ax[2].set_title('Log(x) vs Log(y)')
+ax[2].grid(True, linestyle='--')
+
+plt.tight_layout()
+plt.show()
