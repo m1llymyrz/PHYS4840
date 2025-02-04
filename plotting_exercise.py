ef6925a25ba8d7cf4c9cbaf29d11b3ec18480b0e
Mila Myers <Amelia.myers@live.com>
2025-02-04 15:59:05 -0700

    Update plotting_exercise.py

--- plotting_exercise.py
+++ plotting_exercise.py
@@ -3,7 +3,7 @@
 #####################################
 #
 # Class 5: Linear and Log + Plotting
-# Author: Amelia Myers
+# Author: Amelia "Mila" Myers
 #
 #####################################
 
@@ -15,28 +15,28 @@ import Myers_function_lab as mfl
 x = np.linspace(1, 100, 500)
 y = mfl.y(x)
 
-#transforming x-yspace into logspace
-log_y = np.log10(y)
-
-log_x = np.log10(x)
-
 #creation of 3 plots in 1 window
 fig, ax = plt.subplots(1, 3, figsize=(10,6))
-#linear plot
+#initial linear plot
 ax[0].plot(x, y, label='Linear Plot', color='r', linewidth=5)
 ax[0].set_xlabel('x')
 ax[0].set_ylabel('y')
 ax[0].set_title('Linear Plot')
 ax[0].grid(True, linestyle='--')
 
-#log-log plot
-ax[1].plot(x, log_y, label='Log-Log Plot', color='magenta', linewidth=5)
+#log-log plot of non-transformed function
+ax[1].plot(x, y, label='Log-Log Plot', color='magenta', linewidth=5)
 ax[1].set_xlabel('x')
 ax[1].set_ylabel('log(y)')
+ax[1].set_xscale('log')
+ax[1].set_yscale('log')
 ax[1].set_title('Log-Log Plot')
 ax[1].grid(True, linestyle='--')
 
-#log(x_ vs log(y)
+#transforming x-yspace into logspace
+log_y = np.log10(y)
+log_x = np.log10(x)
+#plot of log(x) vs log(y) with a transformed function
 ax[2].plot(log_x, log_y, label='Log(x) vs Log(y)', color="purple", linewidth=5)
 ax[2].set_xlabel('log(x)')
 ax[2].set_ylabel('log(y)')
