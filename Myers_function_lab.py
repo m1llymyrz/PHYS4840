13d42d9b077ec1a247fa2869ef1c9270199c0b26
Mila Myers <Amelia.myers@live.com>
2025-02-04 15:51:39 -0700

    Create Myers_function_lab.py
    File contains a library of functions created by Amelia "Mila" Myers.

---
+++ Myers_function_lab.py
@@ -0,0 +1,40 @@
+# !/usr/bin/python3.12.3
+#####################################
+#
+# Myers Function Lab Library
+# Author: Amelia "Mila" Myers
+#
+#####################################
+
+import numpy as np
+
+def my_function(vector):
+	a = vector[0]
+	b = vector[1]
+	c = vector[2]
+
+	return np.linalg.norm(vector)
+
+def cookie(m):
+###Function cookie defines how many cookies of a single variety you can buy with m amount of dollars.###
+
+#Below is a list of the cookie varieties and their prices in US dollars in 2025.
+	sugar = 2.65
+	chocolate = 3.2
+	snickerdoodle = 3.45
+	smores = 3.7
+
+#Dictionary setup 'lit' allows for a forloop to be used for ease of coding	
+	lit = {sugar, chocolate, snickerdoodle, smores}
+#For each item in list 'lit':
+#we will divide the price (to the nearest integer) to find how many cookies we can buy with value c1,
+#we will find the amount leftover with value b1.	
+	for i in lit:
+		c1 = m//i
+		b1 = m % (i*c1)
+		print(f'number of cookies: {c1}', f' and the money leftover: ${b1:.3}')
+
+#plotting exercise class 5. 
+def y(x):
+ 	y = 2.0*x**3.0
+ 	return y
