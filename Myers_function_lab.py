89c078cd5013bf31b51ead6c218664490c5b9c4c
Mila Myers <amelia.myers@live.com>
2025-02-13 16:20:33 -0700

    2.13 update 2 with newly added functions

--- Myers_function_lab.py
+++ Myers_function_lab.py
@@ -45,3 +45,16 @@ def distance_modulus(distance):
 	# m - M = 5log.10(d/10)
 	d = 5*np.log10(distance/10)
 	return(round(d,3))
+
+#########################################################
+#
+# This is a function that adds additional tick marks
+# on the figure panels and increases their size and frequency
+# it accepts the argument "ax", which is a figure-like object,
+# and applies formatting to ax
+#
+###########################################################
+def format_axes(ax):
+    ax.tick_params(axis='both', which='major', labelsize=14, length=6, width=1.5)  # Larger major ticks
+    ax.tick_params(axis='both', which='minor', labelsize=12, length=3, width=1)    # Minor ticks
+    ax.minorticks_on()  # Enable minor ticks
