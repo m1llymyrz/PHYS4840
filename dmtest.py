cef94a2f158795065fede49429ef7e7a10dad346
Mila Myers <amelia.myers@live.com>
2025-02-13 15:31:12 -0700

    Test of the newly added Distance Modulus function in the Myers function library.

---
+++ dmtest.py
@@ -0,0 +1,13 @@
+#!/millmyrz/callisto/bin python3.12
+#####################################
+#
+# Distance Modulus test
+# Author: A. Mila Myers
+#
+#####################################
+
+import Myers_function_lab as mfl
+
+x = mfl.distance_modulus(8.63E3)
+
+print(x)
