30c3af68ce2dad70eaaa75c0b9bd5983f163d112
Mila Myers <amelia.myers@live.com>
2025-04-24 14:27:11 -0600

    part of homework 7 problem0

--- Myers_function_lab.py
+++ Myers_function_lab.py
@@ -207,3 +207,25 @@ def euler_method(f, x0, t0, t_end, dt):
     for i in range(1, len(t_values)):
         x_values[i] = x_values[i-1] + dt * f(x_values[i-1], t_values[i-1])
     return t_values, x_values
+
+
+def laplacian_operator(Phi, dx, dy, dz):
+    """
+    Compute the Laplacian of a scalar field Phi (i.e., apply the Poisson operator)
+    using central finite differences on a 3D uniform grid.
+
+    Parameters:
+    - Phi : 3D numpy array of shape (nx, ny, nz)
+    - dx, dy, dz : grid spacings in x, y, z directions
+
+    Returns:
+    - laplacian : 3D numpy array of the same shape as Phi
+    """
+
+    laplacian = (
+        (np.roll(Phi, -1, axis=0) - 2*Phi + np.roll(Phi, 1, axis=0)) / dx**2 +
+        (np.roll(Phi, -1, axis=1) - 2*Phi + np.roll(Phi, 1, axis=1)) / dy**2 +
+        (np.roll(Phi, -1, axis=2) - 2*Phi + np.roll(Phi, 1, axis=2)) / dz**2
+    )
+
+    return laplacian
